5bc3c246d8f52e6516b00126ab523ba62bb7229d
Himanshu-kumar-gupta <himanshu63936gupta@gmail.com>
2020-03-26 23:28:56 +0530

    Commit on branch test

--- program1.cpp
+++ program1.cpp
@@ -6,6 +6,7 @@ int main()
 
 	cout>>"This program is to get started with github organisation">>endl;
 	cout>>"My name is Himanshu kumar gupta">>endl;
+	cout>>"Creating branch and merging1">>endl; 
 	return 0;
 }
 
